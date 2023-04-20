7d8f7efa4bcaf9d00568e06a0e61380e0ba417fb
Tart <eidzen@mail.ru>
2023-04-21 00:21:24 +0300

    Был лютый фикс системы выбора задачи (очень интересный способ получился, но надо будет подумать так лучше сделать и вообще получится ли). Получилось сделать систему выбора ответа голосом (внесло свою лепту в код и БД, но повышает распознавание всего и вся)

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ logging.basicConfig(level=logging.INFO)
 
 sessionStorage = {}
 
+bad_image = '1030494/c85f3ac0666a9fca691b'
+
 
 @app.route('/post', methods=['POST'])
 def main():
@@ -42,7 +44,7 @@ def handle_dialog(res, req):
             res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
         else:
             sessionStorage[user_id]['first_name'] = first_name
-            sessionStorage[user_id]['guessed_cities'] = []
+            sessionStorage[user_id]['guessed_quest'] = []
             res['response']['text'] = f'Приятно познакомиться, {first_name.title()}. Я Алиса. Начнём игру?'
             res['response']['buttons'] = [
                 {
@@ -57,13 +59,15 @@ def handle_dialog(res, req):
     else:
         if not sessionStorage[user_id]['game_started']:
             if 'да' in req['request']['nlu']['tokens']:
-                if len(sessionStorage[user_id]['guessed_cities']) == 3:
+                if len(sessionStorage[user_id]['guessed_quest']) == 10:
                     res['response']['text'] = 'Ты сварил прекрасное зелье!'
                     res['end_session'] = True
                 else:
                     sessionStorage[user_id]['game_started'] = True
                     sessionStorage[user_id]['attempt'] = 1
-                    play_game(res, req)
+                    que = get_quest(list_of_dicts)
+                    sessionStorage[user_id]['quest'] = que
+                    play_game(res, req, sessionStorage[user_id]['quest'])
             elif 'нет' in req['request']['nlu']['tokens']:
                 res['response']['text'] = 'Ну и ладно!'
                 res['end_session'] = True
@@ -80,22 +84,20 @@ def handle_dialog(res, req):
                     }
                 ]
         else:
-            play_game(res, req)
+            play_game(res, req, sessionStorage[user_id]['quest'])
 
 
-def play_game(res, req):
+def play_game(res, req, que):
     user_id = req['session']['user_id']
     attempt = sessionStorage[user_id]['attempt']
+    quest = que
     if attempt == 1:
-        quest = get_quest(list_of_dicts)
-        while quest in sessionStorage[user_id]['quest']:
-            quest = get_quest(list_of_dicts)
         sessionStorage[user_id]['quest'] = quest
         res['response']['card'] = {}
         res['response']['card']['type'] = 'BigImage'
         res['response']['card']['title'] = quest['quest']
         res['response']['card']['image_id'] = quest['image']
-        res['response']['text'] = 'Тогда сыграем!'
+        res['response']['text'] = quest['quest'][:-1:]
         res['response']['buttons'] = [
             {
                 'title': quest['var1'],
@@ -108,51 +110,61 @@ def play_game(res, req):
             {
                 'title': quest['var3'],
                 'hide': True
-            },
+            }
         ]
     else:
-        quest = get_quest(list_of_dicts)
-        if quest in sessionStorage[user_id]['quest']:
-            if req['request']['original_utterance'].lower() == quest['answer'].lower():
-                res['response']['text'] = 'Правильно! Переходим к следующему вопросу?'
-                res['response']['buttons'] = [
-                    {
-                        'title': 'Да',
-                        'hide': True
-                    },
-                    {
-                        'title': 'Нет',
-                        'hide': True
-                    },
-                ]
-            return
-
-        if req['request']['original_utterance'].lower() == quest['answer'].lower():
-            res['response']['text'] = 'Правильно!'
-            sessionStorage[user_id]['quest'].append(quest)
+        print(str(req['request']['original_utterance']).capitalize())
+        print(quest['answer'])
+        if str(req['request']['original_utterance']).capitalize() in str(quest['answer']):
+            res['response']['text'] = 'Правильно! Сыграем ещё?'
+            res['response']['buttons'] = [
+                {
+                    'title': 'Да',
+                    'hide': True
+                },
+                {
+                    'title': 'Нет',
+                    'hide': True
+                }
+            ]
+            sessionStorage[user_id]['guessed_quest'].append(quest)
+            sessionStorage[user_id]['game_started'] = False
             return
         else:
-            res['response']['text'] = 'Неправильно'
             if attempt == 3:
-                res['response']['text'] = f'Вы пытались. Сыграем ещё?'
-                sessionStorage[user_id]['game_started'] = False
-                sessionStorage[user_id]['quest'].append(quest)
-                return
-            else:
+                bad_image
                 res['response']['card'] = {}
                 res['response']['card']['type'] = 'BigImage'
-                res['response']['card']['title'] = 'Неправильно.'
-                res['response']['card']['image_id'] = '12' ## Сюда фигачим айдишник разбитого зелья
-                res['response']['text'] = 'А вот и не угадал!'
-    sessionStorage[user_id]['attempt'] += 1
+                res['response']['card']['title'] = f'Вы пытались. Это {quest["answer"]}. Сыграем ещё?'
+                res['response']['text'] = f'Вы пытались. Это {quest["answer"]}. Сыграем ещё?'
+                res['response']['card']['image_id'] = bad_image
 
+                sessionStorage[user_id]['game_started'] = False
+                sessionStorage[user_id]['guessed_quest'].append(quest)
+                return
+            else:
+                res['response']['text'] = f'Неправильно, подумайте ещё.\n{quest["quest"]}'
+                res['response']['buttons'] = [
+                    {
+                        'title': quest['var1'],
+                        'hide': True
+                    },
+                    {
+                        'title': quest['var2'],
+                        'hide': True
+                    },
+                    {
+                        'title': quest['var3'],
+                        'hide': True
+                    }
+                ]
+    sessionStorage[user_id]['attempt'] += 1
 
 
 def get_first_name(req):
     for entity in req['request']['nlu']['entities']:
         if entity['type'] == 'YANDEX.FIO':
-            return entity['value'].get('first_name', None)\
-
+            return entity['value'].get('first_name', None)
 
 
 def get_quest(sp):
@@ -163,3 +175,6 @@ def get_quest(sp):
     except Exception as e:
         return 'А вопросы кончились'
 
+
+if __name__ == '__main__':
+    app.run()
